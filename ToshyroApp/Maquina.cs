4699dbb77f017780e1dd2017e09b636677e90a99
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-10-17 11:53:56 -0300

    Definido o objeto MAQUINA e as estrutura de dados que armazenarao as principais informacoes

---
+++ ToshyroApp/Maquina.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToshyroApp
+{
+    internal class Maquina
+    {
+        private List<Double> moedasInseridas;
+
+        private Double trocoPendente;
+
+        private Dictionary<Double, int> moedasDaMaquina;
+        private Dictionary<String, (Double preco, int quantidade)> produtosDaMaquina;
+        
+        private readonly Double[] moedasValidas = { 0.01, 0.05, 0.10, 0.25, 0.50, 1.00 };
+
+        public Maquina()
+        {
+            moedasInseridas = new List<Double>();
+
+            trocoPendente = 0.0;
+
+            moedasDaMaquina = new Dictionary<Double, int>
+            {
+                { 0.01, 10 }, { 0.05, 10 }, { 0.10, 10 },
+                { 0.25, 10 }, { 0.50, 10 }, { 1.00, 10 }
+            };
+
+            produtosDaMaquina = new Dictionary<string, (double preco, int quantidade)>
+            {
+                { "Coca-cola",  ( 1.50, 10 ) },
+                { "Água",       ( 1.00, 10 ) },
+                { "Pastelina",  ( 0.30, 10 ) },
+            };
+        }
+    }
+}
